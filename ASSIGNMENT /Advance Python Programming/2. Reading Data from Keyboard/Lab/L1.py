7495cc9b74e1403ffc805c861638c4af6b16e38e
KRISHNA RAJPOPAT <krishnarajpopat@KRISHNA.local>
2025-07-29 12:24:09 +0530

    Assignmnet

---
+++ ASSIGNMENT /Advance Python Programming/2. Reading Data from Keyboard/Lab/L1.py
@@ -0,0 +1,7 @@
+"""Write a Python program to read a name and age from the user and print a formatted output."""
+
+name = input("Enter your name:")
+print(name)
+
+age = int(input("Enter your age:"))
+print(age)
---
+++ ASSIGNMENT /Advance Python Programming/2. Reading Data from Keyboard/Practical/P1.py
@@ -0,0 +1,12 @@
+"""Write a Python program to read a string, an integer, and a float from
+the keyboard and display them."""
+
+name = input("Enter your name:")
+age = int(input("ENter your age:"))
+percentage = float(input("Enter your percentage:"))
+
+print(f"Name : {name}")
+print(f"Age : {age}")
+print(f"Percentage : {percentage}")
+
+
